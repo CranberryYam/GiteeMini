4550d54d1edc6ea98b481996ae63e4d2da9b6578
Henry Yi <1113145615yihl@gmail.com>
2021-05-06 18:41:32 +0800

    Polish card component

--- components/card/card.js
+++ components/card/card.js
@@ -22,7 +22,16 @@ Component({
         },
         labels: { 
             type: Array,
-            value: ['bug']
+            value: ['bug', 'test', 'performance', 'task', 'serious']
         },
+    },
+    lifetimes: {
+        attached: function() {
+            // 在组件实例进入页面节点树时执行
+        }
+    },
+    // 以下是旧式的定义方式，可以保持对 <2.2.3 版本基础库的兼容
+    attached: function() {
+    
     }
 })
