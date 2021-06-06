5bbdc3815799e3162b71c03e2ea358e97044e45e
Henry Yi <1113145615yihl@gmail.com>
2021-06-06 20:09:36 +0800

    Add single issue page

---
+++ miniprogram/pages/issue/issue.js
@@ -0,0 +1,19 @@
+Page({
+
+    /**
+     * Page initial data
+     */
+    data: {
+        issue: {
+            name: "在线 `Furion` 代码生成器开发",
+            body: "# 天气api"
+        }
+    },
+  
+    /**
+     * Lifecycle function--Called when page load
+     */
+    onLoad: function (options) {
+    }
+
+})
